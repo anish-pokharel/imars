4c2d5c71836ef459b0a662315ce910db8617a3f9
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-06-07 00:30:34 +0545

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login-page', component: LoginPageComponent },
+  { path: 'main-page', component: MainPageComponent },
+  { path: 'registration-page', component: RegistrationPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegistrationPageComponent,
+    MainPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
