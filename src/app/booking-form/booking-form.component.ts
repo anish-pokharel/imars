837b21225888f65f9a8b121cff23523104c7c2d4
Menuka Lamsal <lamshalashmita2020@gmail.com>
2023-06-19 21:44:47 +0545

    Login-page & Register page

--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.scss']
 })
-export class BookingFormComponent {
+export class BookingFormComponent implements OnInit {
   showModalFlag: boolean = false;
   otpValue: string = '';
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: HttpClient) {
 
   }
   actionBooking() {
@@ -29,5 +30,8 @@ export class BookingFormComponent {
   hideModal() {
     this.showModalFlag = false;
     this.otpValue = '';
+  }
+  ngOnInit(): void {
+      
   }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
@@ -19,9 +20,9 @@ export class LoginPageComponent implements OnInit {
   showOptField=false;
   submitted=false;
 
-  constructor(private router: Router,private fb:FormBuilder){
+  constructor(private router: Router,private fb:FormBuilder,private http: HttpClient){
     this.loginForm = this.fb.group({
-      email: ['', Validators.required],
+      Email: ['', Validators.required],
       Password: ['', Validators.required],
       // password: new FormControl('', [Validators.required, Validators.minLength(8)])
     });
@@ -35,7 +36,23 @@ export class LoginPageComponent implements OnInit {
 
   }
   formSubmit(){
-    this.router.navigate(['/main-page'])  
+
+    const formData = this.loginForm.value;
+    this.http.post('http://localhost:3000/login-page', formData).subscribe(
+      (response) => {
+        console.log('Login successfull');
+        if(this.loginForm.value.Email == "admin@gmail.com" && this.loginForm.value.Password == "admin@123"){
+          this.router.navigate(['./admin-dashboard']);
+        }else{
+          this.router.navigate(['./main-page']);
+        }
+        
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Error loggingin:', error);
+        alert('Error Loggingin. Please try again later.');
+      }
+    );
   }
   forgetPasswordModal(){}
 
