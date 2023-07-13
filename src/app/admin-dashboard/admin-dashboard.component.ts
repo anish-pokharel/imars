fdef05174b9043f6530a6fdadb2f42e99cfded42
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-07-13 21:52:11 +0545

    home

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -35,37 +35,36 @@ export class AdminDashboardComponent implements OnInit {
       console.error('Token not found');
       return;
     }
-    
-   /* const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    this.http.get<any>('http://localhost:3000/authenticateUser', { headers }).subscribe(
-      (response) => {
-        this.token = response.token;
-        this.getContacts(); // Call the method to fetch contacts after successful token verification
-      },
-      (error) => {
-        console.error('Error retrieving token:', error);
-      }
-    );*/
+
+    /* const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+     this.http.get<any>('http://localhost:3000/authenticateUser', { headers }).subscribe(
+       (response) => {
+         this.token = response.token;
+         this.getContacts(); // Call the method to fetch contacts after successful token verification
+       },
+       (error) => {
+         console.error('Error retrieving token:', error);
+       }
+     );*/
   }
   getContacts(): void {
     const token = localStorage.getItem('token');
     console.log('Token stored:', token);
     debugger
     let headers = new HttpHeaders();
-if (token) {
-  headers = headers.set("Authorization", token);
-}
-   debugger
+    if (token) {
+      headers = headers.set("Authorization", token);
+    }
+    debugger
     console.log(headers);
     debugger
-    this.http.get<any>('http://localhost:3000/admin-dashboard', { headers : headers}).subscribe(
+    this.http.get<any>('http://localhost:3000/admin-dashboard', { headers: headers }).subscribe(
       (response) => {
         debugger
         this.contacts = response.contacts;
         debugger
-        console.log("contact retrieved!!",response.contacts);
+        console.log("contact retrieved!!", response.contacts);
         debugger
-        // Perform any necessary operations with the fetched contacts
       },
       (error) => {
         console.error('Error fetching contacts:', error);
@@ -73,55 +72,22 @@ if (token) {
     );
     debugger
   }
-  
-  // onSubmit() {
-  //   this.dataservice.registerAdmin(this.formData).subscribe(
-  //     (response) => {
-  //       console.log('Registration successful');
-  //       this.formData = {};
-  //     },
-  //     (error) => {
-  //       console.error('Error registering admin:', error);
-  //     }
-  //   );
-  // }
-  // onSubmit(data: any): void {
-  //   const token = localStorage.getItem('token');
-  //   console.log('Token stored:', token);
-  //   debugger
-  //   // Set the Authorization header with the retrieved token
-  //   // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  //   // const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  //   const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-  //   console.log(headers)
-  //   debugger
-  //   // Make an HTTP request to save the data to the backend API
-  //   this.http.post('http://localhost:3000/admin-dashboard', data, { headers }).subscribe(
-  //     (response) => {
-  //       console.log('Data saved successfully');
-  //     },
-  //     (error) => {
-  //       console.error('Error registering admin:', error);
-  //     }
-  //   );
-  // }
   onSubmit(): void {
     const token = localStorage.getItem('token');
     console.log('Token stored:', token);
     debugger
     let headers = new HttpHeaders();
-if (token) {
-  headers = headers.set("Authorization", token);
-}
-   debugger
+    if (token) {
+      headers = headers.set("Authorization", token);
+    }
+    debugger
     console.log(headers);
     debugger
-    // Make an HTTP request to save the data to the backend API
-    this.http.post('http://localhost:3000/admin-dashboard', this.formData, {headers : headers}).subscribe(
+    this.http.post('http://localhost:3000/admin-dashboard', this.formData, { headers: headers }).subscribe(
       (response) => {
         console.log('Data saved successfully');
-        
+
       },
       (error) => {
         console.error('Error registering admin:', error);
@@ -134,12 +100,12 @@ if (token) {
     this.currentSection = section;
   }
 
-  acceptRequests(request: any) :void{
+  acceptRequests(request: any): void {
     this.acceptedRequests.push(request);
     this.rejectedRequests = this.rejectedRequests.filter(r => r !== request);
 
   }
-  rejectRequests(request: any):void {
+  rejectRequests(request: any): void {
     this.rejectedRequests.push(request);
     this.acceptedRequests = this.acceptedRequests.filter(r => r !== request);
 
