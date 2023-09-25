5757498d066cbb6009a832da19af6ca3992de5e7
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-09-26 00:11:04 +0545

    service

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../api/service/data.service';
-import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -10,8 +10,8 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AdminDashboardComponent implements OnInit {
   currentSection: string = 'basic';
-  bookingForm: any[]=[];
-  acceptedRequests:any[];
+  bookingForm: any[] = [];
+  acceptedRequests: any[];
   rejectedRequests: any[];
   formData: any = {};
   token: string = '';
@@ -38,40 +38,29 @@ export class AdminDashboardComponent implements OnInit {
       console.error('Token not found');
       return;
     }
-   // document.cookie = `jwt=${token}`; // Set the token as a cookie
 
-   /* const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    this.http.get<any>('http://localhost:3000/authenticateUser', { headers }).subscribe(
-      (response) => {
-        this.token = response.token;
-        this.getContacts(); // Call the method to fetch contacts after successful token verification
-      },
-      (error) => {
-        console.error('Error retrieving token:', error);
-      }
-    );*/
   }
   getContacts(): void {
 
     const token = this.cookieService.get('jwt');
 
-  if (!token) {
-    console.error('Token not found');
-    return;
-  }
-  const headers = new HttpHeaders({
-    'Authorization': `Bearer ${token}`
-  });
+    if (!token) {
+      console.error('Token not found');
+      return;
+    }
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
 
-    console.log("header",headers);
-    this.http.get<any>('http://localhost:3000/admin-dashboard',{ headers:headers }).subscribe(
+    console.log("header", headers);
+    this.http.get<any>('http://localhost:3000/admin-dashboard', { headers: headers }).subscribe(
       (response) => {
         debugger
         this.contacts = response.contacts;
-        this.bookingForm=response.bookingForm;
+        this.bookingForm = response.bookingForm;
         debugger
-        console.log("contact retrieved!!",this.contacts);
-        console.log("accepted buses retrieved!!",this.bookingForm);
+        console.log("contact retrieved!!", this.contacts);
+        console.log("accepted buses retrieved!!", this.bookingForm);
         debugger
         // Perform any necessary operations with the fetched contacts
       },
@@ -81,45 +70,13 @@ export class AdminDashboardComponent implements OnInit {
     );
     debugger
   }
-  
-  // onSubmit() {
-  //   this.dataservice.registerAdmin(this.formData).subscribe(
-  //     (response) => {
-  //       console.log('Registration successful');
-  //       this.formData = {};
-  //     },
-  //     (error) => {
-  //       console.error('Error registering admin:', error);
-  //     }
-  //   );
-  // }
-  // onSubmit(data: any): void {
-  //   const token = localStorage.getItem('token');
-  //   console.log('Token stored:', token);
-  //   debugger
-  //   // Set the Authorization header with the retrieved token
-  //   // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  //   // const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  //   const headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-  //   console.log(headers)
-  //   debugger
-  //   // Make an HTTP request to save the data to the backend API
-  //   this.http.post('http://localhost:3000/admin-dashboard', data, { headers }).subscribe(
-  //     (response) => {
-  //       console.log('Data saved successfully');
-  //     },
-  //     (error) => {
-  //       console.error('Error registering admin:', error);
-  //     }
-  //   );
-  // }
   onSubmit(): void {
     // Make an HTTP request to save the data to the backend API
-    this.http.post('http://localhost:3000/admin-dashboard', this.formData, ).subscribe(
+    this.http.post('http://localhost:3000/admin-dashboard', this.formData,).subscribe(
       (response) => {
         console.log('Data saved successfully');
-        
+
       },
       (error) => {
         console.error('Error registering admin:', error);
@@ -132,12 +89,12 @@ export class AdminDashboardComponent implements OnInit {
     this.currentSection = section;
   }
 
-  acceptRequests(request: any) :void{
+  acceptRequests(request: any): void {
     this.acceptedRequests.push(request);
     this.rejectedRequests = this.rejectedRequests.filter(r => r !== request);
 
   }
-  rejectRequests(request: any):void {
+  rejectRequests(request: any): void {
     this.rejectedRequests.push(request);
     this.acceptedRequests = this.acceptedRequests.filter(r => r !== request);
 
