43c3f06a570b310ed2ca29b7ae3879864e2e48f8
Menuka Lamsal <lamshalashmita2020@gmail.com>
2023-09-29 00:53:50 +0545

    user registration successfull

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-registration-page',
@@ -14,51 +15,61 @@ export class RegistrationPageComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient) {
     this.registerForm = this.formBuilder.group({
-      Name: ['', Validators.required],
-      Email: ['', [Validators.required, Validators.email]],
-      Address: ['', Validators.required],
-      Phone: ['', Validators.required],
-      Gender: [false, Validators.requiredTrue],
-      Password: ['', Validators.required],
-      ConfirmPassword: ['', Validators.required],
+      Name: [''],
+      Email: [''],
+      Address: [''],
+      Phone: [0],
+      Gender: [false],
+      Password: [''],
+      ConfirmPassword: [''],
       //AgreementCheckbox: [false, Validators.requiredTrue]
     });
   }
 
 
   onSubmit() {
-    if (this.registerForm.value.Password === this.registerForm.value.ConfirmPassword) {
-      console.log('Password matched');
-
-
-      if (!this.registerForm.invalid) {
-        alert("Please enter valid information.");
-        return;
-
-      }
-
-
-      // if (!this.registerForm.value.agreementCheckbox) {
-      // alert("Please agree to the Terms and Data Policy.");
-      // return;
-      // }
-      const formData = this.registerForm.value;
-      this.http.post('http://localhost:3000/registration-page', formData).subscribe(
-        (response) => {
-          console.log('User registered successfully');
-          this.router.navigate(['./login-page']);
-        },
-        (error: HttpErrorResponse) => {
-          console.error('Error registering user:', error);
-          alert('Error registering user. Please try again later.');
+    if (this.registerForm.value.Password.length>=6 ) {
+     
+      if(this.registerForm.value.Password === this.registerForm.value.ConfirmPassword){
+        console.log('Password matched');
+        if( this.registerForm.value.Phone>0){
+          // if (!this.registerForm.invalid) {
+          //   alert("Please enter valid information.");
+          //   return;
+    
+          // }
+    
+    
+          // if (!this.registerForm.value.agreementCheckbox) {
+          // alert("Please agree to the Terms and Data Policy.");
+          // return;
+          // }
+          const formData = this.registerForm.value;
+          this.http.post('http://localhost:3000/registration-page', formData).subscribe(
+            (response) => {
+              alert("User registered successfully");
+              console.log('User registered successfully');
+              this.router.navigate(['./login-page']);
+            },
+            (error: HttpErrorResponse) => {
+              console.error('Error registering user:', error);
+              alert("Error registering user");
+            }
+          );
+        }else{
+            alert("Mobile Number must be 10 digits and not zero");
         }
-      );
+      }
+      else{
+      alert("Passwords do not match");
+      }
     }
     else {
-      alert('Passwords do not match');
+      alert('Password must be atleast 6 digits');
     }
-  }
-  ngOnInit(): void {
+}
+
+ngOnInit(): void {
 
   }
 
